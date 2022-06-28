a45f9e47f28a2c71f119be261d9fb0ea9b891e02
Triggy <93479838+litriggy@users.noreply.github.com>
2022-06-28 19:16:41 +0900

    Update twilio.js

--- services/twilio.js
+++ services/twilio.js
@@ -1,20 +1,17 @@
-const accountSid = "ACcc50295831df1a796cc95cad7b6c5e29"
-const authToken = "0eb17564b185d63a59efc9742ac85255"
+const accountSid = ""
+const authToken = ""
 const client = require('twilio')(accountSid, authToken);
 
 
 const sendMessage=(phone, CODE)=>{
-    console.log("asdfasdfsadf",phone)
     client.messages
     .create({
         body: `[BETBIT] Requested access code is [${CODE}]`,
-        messagingServiceSid: 'MGbd6fe115a5e00934c9b876e2e07748af',   
+        messagingServiceSid: "",   
         from: "+13257398298",
         to: phone
     })
     .then(message =>{
-        
-        console.log("asdf",message)
         return message.sid});
 }
 
